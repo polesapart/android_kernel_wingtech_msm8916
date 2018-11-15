a440eaaa108637d9207d84942a0ed48fed6972cd
Alexandre Pereira Nunes <alexandre.nunes@gmail.com>
2018-11-15 09:29:53 -0200

    Don't touch wifi firmware anymore. Less intrusive cpu scaling  (not ideal yet).

--- AnyKernel2/anykernel.sh
+++ AnyKernel2/anykernel.sh
@@ -17,6 +17,7 @@ device.name5=
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
+ramdisk_compression=auto;
 
 
 ## AnyKernel methods (DO NOT CHANGE)
@@ -40,10 +41,10 @@ insert_line /system/vendor/etc/init/hw/init.target.rc "/system/vendor/etc/fstab.
 insert_line /system/vendor/etc/init/hw/init.target.rc "/system/vendor/etc/fstab.qcom" before "on property:sys.ims.QMI_DAEMON_STATUS=1" "on property:sys.boot_completed=1"
 insert_line /system/vendor/etc/init/hw/init.target.rc "/system/vendor/etc/fstab.qcom" after "boot_completed=1" "  swapon_all /system/vendor/etc/fstab.qcom\n"
 replace_string /system/vendor/etc/init/hw/init.qcom.power.rc "scaling_min_freq 200000" "scaling_min_freq 800000" "scaling_min_freq 200000"
-replace_string /system/vendor/etc/init/hw/init.qcom.power.rc "target_loads \"50 " "target_loads.*$" "target_loads \"50 400000:55 533333:60 800000:65 998400:70 1094400:80 11520000:90\""
+replace_string /system/vendor/etc/init/hw/init.qcom.power.rc "target_loads \"10 " "target_loads.*$" "target_loads \"10 533333:60 800000:85 998400:90 1094400:80\""
 replace_file /system/vendor/etc/wifi/WCNSS_qcom_cfg.ini 644 WCNSS_qcom_cfg-prima.ini
-replace_file /system/vendor/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin 644 WCNSS_qcom_wlan_nv.bin
-replace_file /system/vendor/firmware/wlan/prima/WCNSS_cfg.dat 644 WCNSS_cfg.dat
+#replace_file /system/vendor/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin 644 WCNSS_qcom_wlan_nv.bin
+#replace_file /system/vendor/firmware/wlan/prima/WCNSS_cfg.dat 644 WCNSS_cfg.dat
 mount -o ro,remount -t auto /system
 # end ramdisk changes
 
