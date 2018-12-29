18301bbb8b5c1b774eb4787847510ec8d86c0172
Alexandre Pereira Nunes <alexandre.nunes@gmail.com>
2018-12-29 15:03:44 -0200

    Change min freq from 200000 -> 533333 & revert part of scaling

--- AnyKernel2/anykernel.sh
+++ AnyKernel2/anykernel.sh
@@ -34,14 +34,14 @@ chmod -R 755 $ramdisk
 dump_boot;
 # begin ramdisk changes
 mount -o rw,remount -t auto /system
-insert_line /system/vendor/etc/fstab.qcom "zram0" after "do specify MF_CHECK" "/dev/block/zram0\tnone\tswap\tdefaults\tzramsize=536870912"
+insert_line /system/vendor/etc/fstab.qcom "zram0" after "do specify MF_CHECK" "/dev/block/zram0\tnone\tswap\tdefaults\tzramsize=402653184"
 insert_line /system/vendor/etc/init/hw/init.target.rc "on init" before "on boot" "on init"
 insert_line /system/vendor/etc/init/hw/init.target.rc "/system/vendor/etc/fstab.qcom" after "on init" " write /sys/block/zram0/comp_algorithm lz4"
 insert_line /system/vendor/etc/init/hw/init.target.rc "/system/vendor/etc/fstab.qcom" after "write /sys/block/zram0" " write /proc/sys/vm/page-cluster 0\n"
 insert_line /system/vendor/etc/init/hw/init.target.rc "/system/vendor/etc/fstab.qcom" before "on property:sys.ims.QMI_DAEMON_STATUS=1" "on property:sys.boot_completed=1"
 insert_line /system/vendor/etc/init/hw/init.target.rc "/system/vendor/etc/fstab.qcom" after "boot_completed=1" "  swapon_all /system/vendor/etc/fstab.qcom\n"
-replace_string /system/vendor/etc/init/hw/init.qcom.power.rc "scaling_min_freq 200000" "scaling_min_freq 800000" "scaling_min_freq 200000"
-replace_string /system/vendor/etc/init/hw/init.qcom.power.rc "target_loads \"10 " "target_loads.*$" "target_loads \"10 533333:60 800000:85 998400:90 1094400:80\""
+replace_string /system/vendor/etc/init/hw/init.qcom.power.rc "scaling_min_freq 533333" "scaling_min_freq.*$" "scaling_min_freq 533333"
+replace_string /system/vendor/etc/init/hw/init.qcom.power.rc "target_loads \"5 " "target_loads.*$" "target_loads \"5 800000:65 998400:70 1094400:80\""
 replace_file /system/vendor/etc/wifi/WCNSS_qcom_cfg.ini 644 WCNSS_qcom_cfg-prima.ini
 #replace_file /system/vendor/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin 644 WCNSS_qcom_wlan_nv.bin
 #replace_file /system/vendor/firmware/wlan/prima/WCNSS_cfg.dat 644 WCNSS_cfg.dat
